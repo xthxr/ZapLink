0e4e33f4008bc5cae892ecb611ac7186555f0a25
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-07-27 10:19:11 +0530

    fix: bugs

--- server.js
+++ server.js
@@ -62,8 +62,8 @@ try {
   db = admin.firestore();
   auth = admin.auth();
   firebaseState.enabled = true;
-firebaseState.mode = 'firestore';
-firebaseState.reason = 'Firebase connected successfully';
+  firebaseState.mode = 'firestore';
+  firebaseState.reason = 'Firebase connected successfully';
 
 
   
